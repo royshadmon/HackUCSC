8b0f3be78a896f304625ceac5399eebe7266f246
royshadmon <royshadmon@Roys-MacBook-Pro.local>
2017-01-21 04:06:26 -0800

    Working on script

--- guests.sql
+++ guests.sql
@@ -1,3 +1,4 @@
-INSERT INTO TABLE Guests VALUES( Roy  , Shadmon );
-INSERT INTO TABLE Guests VALUES( Alon , Peckorovsky );
-INSERT INTO TABLE Guests VALUES( Ari  , GoldStein );
+DROP TABLE IF EXISTS Guests;
+CREATE TABLE Guests (firstname char(30), lastname char(30), time_entered TIMESTAMP, PRIMARY KEY(firstname, lastname));
+INSERT INTO Guests VALUES('ALON','PEKUROVSKY','2017-01-21 03:54:39');
+INSERT INTO Guests VALUES('ROY','SHADMON','2017-01-21 03:54:42');
